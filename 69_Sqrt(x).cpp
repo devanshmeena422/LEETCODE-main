9497fb325067f16ec5e263c2bac1323247105e84
Devansh Meena <devanshmeena422@gmail.com>
2026-03-26 17:33:51 +0530

    Leetcode

---
+++ 69_Sqrt(x).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        long left = 0, right = x;
+        
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            
+            if (mid * mid == x)
+                return mid;
+            
+            else if (mid * mid < x)
+                left = mid + 1;
+            
+            else
+                right = mid - 1;
+        }
+        
+        return right;
+    }
+};
