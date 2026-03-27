c745bf3cd92acc2753d1fb4a97a5eb9bbea78132
Devansh Meena <devanshmeena422@gmail.com>
2026-03-27 09:28:52 +0530

    Leetcode

---
+++ 389_Find_the_Differnce.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char se;
+        int freq[26]={0};
+        for(int i=0;i<t.size();i++){
+            freq[t[i]-'a']++;
+        }
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]==1){
+                se='a'+i;
+            }
+        }
+        return se;
+    }
+};
